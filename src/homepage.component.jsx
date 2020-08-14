8fcd8e39b1cb1893e94e3c806b9f06c6cbe53cb2
Gerben van de Weerdhof <gerben1978@hotmail.com>
2020-08-15 00:51:52 +0200

    created react app and added files for homepage

---
+++ src/homepage.component.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import "./homepage.styles.scss";
+
+const HomePage = () => (
+  <div className="homepage">
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">FILMS</h1>
+          <span className="subtitle">NAAR COLLECTIE</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">GAMES</h1>
+          <span className="subtitle">NAAR COLLECTIE</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">BOEKEN</h1>
+          <span className="subtitle">NAAR COLLECTIE</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">COMPUTER</h1>
+          <span className="subtitle">NAAR COLLECTIE</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">KLEDING</h1>
+          <span className="subtitle">NAAR COLLECTIE</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">SCHOENEN</h1>
+          <span className="subtitle">NAAR COLLECTIE</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">WONEN</h1>
+          <span className="subtitle">NAAR COLLECTIE</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">TUIN</h1>
+          <span className="subtitle">NAAR COLLECTIE</span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default HomePage;
